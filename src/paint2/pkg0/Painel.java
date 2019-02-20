905a14104cbf3bc198c9fe8064d4fd8b8c87403a
deadlyshoes <36314990+deadlyshoes@users.noreply.github.com>
2019-02-19 22:07:40 -0300

    Update Painel.java

--- src/paint2/pkg0/Painel.java
+++ src/paint2/pkg0/Painel.java
@@ -17,6 +17,7 @@ import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+import java.util.ArrayList;
   
         
 public class Painel extends JPanel {
@@ -24,9 +25,12 @@ public class Painel extends JPanel {
     private int y;
     private int altura;
     private int largura;
-    private Forma[] formas = new Forma[100];
-    private int ultimoObj;
-    private Color[] cores = {color.BLACK, color.BLUE};
+    private String tipoForma;
+    private ArrayList<Forma> formas = new ArrayList();
+    private Color[] cores = {Color.BLACK, Color.BLUE, Color.CYAN, 
+                             Color.GRAY, Color.GREEN, Color.LIGHT_GRAY,
+                             Color.MAGENTA, Color.ORANGE, Color.PINK,
+                             Color.RED, Color.WHITE, Color.YELLOW};
     private int idCor;
     
     public Painel() {
@@ -34,44 +38,57 @@ public class Painel extends JPanel {
             public void mousePressed(MouseEvent e) {
                 x = e.getX();
                 y = e.getY();
+                formas.add(new Retângulo(0, 0, null, 0, 0)); //Dummy
             }
         });
         addMouseMotionListener(new MouseMotionAdapter() {
             public void mouseDragged(MouseEvent e) {
-                //Adicionar preview
+                int tmpX = Math.min(x, e.getX());
+                int tmpY = Math.min(y, e.getY());
+                largura = Math.abs(x - e.getX());
+                altura = Math.abs(y - e.getY());
+                Retângulo formaNova = new Retângulo(tmpX, tmpY, cores[idCor], largura, altura);
+                /**switch (tipoForma) {
+                    case "Circunferência": formaNova = (Circunferência) formaNova;
+                    case "Elipse": formaNova = (Elipse) formaNova;
+                    case "Triângulo": formaNova = (Triângulo) formaNova;
+                } **/
+                formas.set(formas.size() - 1, formaNova);
+                repaint();
             }
         });
         addMouseListener(new MouseAdapter() {
             public void mouseReleased(MouseEvent e) {
-                largura = Math.abs(x - e.getX());
-                altura = Math.abs(y - e.getY());
-                x = Math.min(x, e.getX());
-                y = Math.min(y, e.getY());
-                formas[ultimoObj] = new Retângulo(x, y, cores[idCor], largura, altura);
-                ultimoObj += 1;
-                repaint();
             }
         });
     }
     
     public void mudarCor(int i) {
-        idCor = i;
+       idCor = i;
+    }
+    
+    public void mudarForma(String f) {
+        tipoForma = f;
     }
 
-    public void removerForma(int x, int y) {
+    /**public void removerForma(int x, int y) {
+     * Fix
         Retângulo[] novasFormas = new Retângulo[100];
-        for (int i=0; i < ultimoObj; i++)
+        for (Object object : col) {
+            
+        }
+        }
             if (x < formas[i].getX() && x > (formas[i].getX() + formas[i].getLargura()))
                 novasFormas[i] = formas[i];
         formas = novasFormas;
         repaint();
-    }
+    } **/
     
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        for (int i=0; i < ultimoObj; i++)
-            if (formas[i] != null)
-                formas[i].Desenhar(g);
+        for (Forma forma : formas) {
+            forma.Desenhar(g);
+        }
     }
 }
