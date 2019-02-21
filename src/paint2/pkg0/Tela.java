eeb19dbe7d9acf9b567602773d0b2e465af03c06
deadlyshoes <deadlymorgar@gmail.com>
2019-02-21 11:00:13 -0300

    Tema da Interface

--- src/paint2/pkg0/Tela.java
+++ src/paint2/pkg0/Tela.java
@@ -321,7 +321,7 @@ public class Tela extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("SystemLookAndFeel".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
