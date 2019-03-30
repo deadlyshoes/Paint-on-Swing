54172b1d218f23d3cfbd1bfcb969883af2525c83
deadlyshoes <hildemar@airmail.cc>
2019-03-30 18:00:51 -0300

    save

--- src/paint2/pkg0/JListCustom.java
+++ src/paint2/pkg0/JListCustom.java
@@ -25,20 +25,22 @@ public class JListCustom<Forma> extends JList {
      * @return 
      */
     public boolean moverCima() {
-        int i = formas.size() - 1 - getSelectedIndex();
+        try {
+            int i = formas.size() - 1 - getSelectedIndex();
         
-        if (getSelectedIndex() > 0) {
-            Forma itemSelecionado = formas.get(i);
-            formas.remove(i);
-            formas.add(i + 1, itemSelecionado);
-            int selectedIndex = getSelectedIndex();
-            listModel.clear();
-            for (Forma forma : formas) {
-                listModel.add(0, forma);
+            if (getSelectedIndex() > 0) {
+                Forma itemSelecionado = formas.get(i);
+                formas.remove(i);
+                formas.add(i + 1, itemSelecionado);
+                int selectedIndex = getSelectedIndex();
+                listModel.clear();
+                for (Forma forma : formas) {
+                    listModel.add(0, forma);
+                }
+                setSelectedIndex(selectedIndex - 1);
+                return true;
             }
-            setSelectedIndex(selectedIndex - 1);
-            return true;
-        }
+        } catch (IndexOutOfBoundsException ex) {}
         return false;
     }
     
@@ -47,20 +49,22 @@ public class JListCustom<Forma> extends JList {
      * @return 
      */
     public boolean moverBaixo() {   
-        int i = formas.size() - 1 - getSelectedIndex();
+        try {
+            int i = formas.size() - 1 - getSelectedIndex();
         
-        if (getSelectedIndex() < listModel.getSize() - 1) {
-            Forma itemSelecionado = formas.get(i);
-            formas.remove(i);
-            formas.add(i - 1, itemSelecionado);
-            int selectedIndex = getSelectedIndex();
-            listModel.clear();
-            for (Forma forma : formas) {
-                listModel.add(0, forma);
+            if (getSelectedIndex() < listModel.getSize() - 1) {
+                Forma itemSelecionado = formas.get(i);
+                formas.remove(i);
+                formas.add(i - 1, itemSelecionado);
+                int selectedIndex = getSelectedIndex();
+                listModel.clear();
+                for (Forma forma : formas) {
+                    listModel.add(0, forma);
+                }
+                setSelectedIndex(selectedIndex + 1);
+                return true;
             }
-            setSelectedIndex(selectedIndex + 1);
-            return true;
-        }
+        } catch (IndexOutOfBoundsException ex) {}
         return false;
     }
 }
